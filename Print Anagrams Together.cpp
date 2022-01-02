5148c9589ef32c03f259dd05eb8ddd34c88ead56
Khusboo Bothra <bothrakhushboo2405@gmail.com>
2022-01-03 02:05:20 +0530

    Create Q1.cpp

---
+++ Print Anagrams Together.cpp
@@ -0,0 +1,32 @@
+/* 
+Problem : Given an array of strings, return all groups of strings that are anagrams. The groups must be created in order of their appearance in the original array.
+Look at the sample case for clarification.
+*/
+
+Solution : 
+vector<vector<string> > Anagrams(vector<string>& string_list) {
+        //code here
+        
+        unordered_map<string,vector<string>>mp;
+        
+        for(auto s:string_list)
+        {
+            string t=s;
+            sort(t.begin(),t.end());
+            mp[t].push_back(s);
+        }
+        vector<vector<string>>ans(mp.size());
+        int idx=0;
+        for(auto it:mp)
+        {
+            for(auto s:it.second)
+            {
+                ans[idx].push_back(s);
+            }
+            idx++;
+        }
+        return ans;
+
+Time Complexity : O(N * (L * log(L))) , where L is the length of the respective strings.
+
+Space Complexity: O(N * L) , where N is the number of words and L is the max length among the given string lengths.
